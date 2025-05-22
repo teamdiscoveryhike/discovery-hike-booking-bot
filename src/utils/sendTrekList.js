f406b81372ae6b0b06518af6f7432a6a2e99ae58
teamdiscoveryhike <team.discoveryhike@gmail.com>
2025-05-22 23:38:00 +0530

    Update sendTrekList.js

--- src/utils/sendTrekList.js
+++ src/utils/sendTrekList.js
@@ -1,1 +1,20 @@
 
+import { sendList } from "../services/whatsapp.js";
+
+export default async function sendTrekList(userId) {
+  return sendList(userId, "Choose Trek/Expedition:", [
+    {
+      title: "Popular Treks",
+      rows: [
+        { id: "Kedarkantha", title: "Kedarkantha Trek" },
+        { id: "Brahmatal", title: "Brahmatal Trek" },
+        { id: "BaliPass", title: "Bali Pass Trek" },
+        { id: "BlackPeak", title: "Black Peak Expedition" },
+        { id: "BorasuPass", title: "Borasu Pass Trek" },
+        { id: "DumdarkandiPass", title: "Dumdarkandi Pass Trek" },
+        { id: "HarKiDun", title: "Har Ki Dun Trek" }
+      ]
+    }
+  ], "🌄 Select a Trek/Expedition");
+}
+
