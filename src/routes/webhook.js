c6e9e2b0b4005a5d7504e2c602761c70bb05015a
teamdiscoveryhike <team.discoveryhike@gmail.com>
2025-05-22 22:33:54 +0530

    Add files via upload

--- src/routes/webhook.js
+++ src/routes/webhook.js
@@ -1,1 +1,7 @@
 
+import express from "express";
+import handleMessage from "../handlers/handleMessage.js";
+
+const router = express.Router();
+router.post("/", handleMessage);
+export default router;
