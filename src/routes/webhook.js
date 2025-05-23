9fbdbc00f557e1859b1d50b84355e20765d79211
teamdiscoveryhike <team.discoveryhike@gmail.com>
2025-05-23 08:16:19 +0530

    Delete src/routes directory

--- src/routes/webhook.js
+++
@@ -1,7 +0,0 @@
-
-import express from "express";
-import handleMessage from "../handlers/handleMessage.js";
-
-const router = express.Router();
-router.post("/", handleMessage);
-export default router;
