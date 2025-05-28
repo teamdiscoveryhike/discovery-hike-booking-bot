6844c8fa29b3fdec03aeccc3ee8653124bffcef0
teamdiscoveryhike <team.discoveryhike@gmail.com>
2025-05-28 23:22:21 +0530

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,1 +1,10 @@
 
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_API_KEY
+);
+
+export default supabase;
+
