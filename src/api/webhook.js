44b8aafd4125c3ff085e3cace8295e2de6c27899
teamdiscoveryhike <team.discoveryhike@gmail.com>
2025-06-12 09:22:35 +0530

    mi

--- src/api/webhook.js
+++ src/api/webhook.js
@@ -12,7 +12,9 @@ import {
   clearEditingFlag,
   getSessionObject,
   setEditPage,
-  getEditPage
+  getEditPage,
+  setSessionData
+
 } from "../services/sessionManager.js";
 import { sendBookingConfirmationEmail } from "../services/email.js";
 import { cancelVoucherSession } from "../services/voucherSessionManager.js";
